b2827ec97f88537ff19f75f76501be3f0ba94e41
HamletJr <jogosboy1902@gmail.com>
2025-04-09 00:19:10 +0700

    feat: add test routes for db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 #[macro_use] extern crate rocket;
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::sqlx::{self, Row};
+use dotenvy::dotenv;
+
+#[derive(Database)]
+#[database("buildingstore")]
+pub struct BuildingStoreDB(sqlx::PgPool);
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/db")]
+async fn test_db(mut db: Connection<BuildingStoreDB>) -> Option<String> {
+    let row = sqlx::query("SELECT * FROM users LIMIT 1")
+        .fetch_one(&mut **db)
+        .await
+        .ok()?;
+
+    let id: i64 = row.get("id");
+    let email: String = row.get("email");
+
+    Some(format!("Hello, {}! Your ID is {}.", email, id))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    dotenv().ok();
+    rocket::build()
+        .manage(reqwest::Client::builder().build().unwrap())
+        .attach(BuildingStoreDB::init())
+        .mount("/", routes![index, test_db])
 }
