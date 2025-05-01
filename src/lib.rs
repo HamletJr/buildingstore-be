4eae1642c96289a1e7c8f2fa925db4350116e0fb
HamletJr <jogosboy1902@gmail.com>
2025-05-01 17:17:30 +0700

    [REFACTOR] Move old DB config to lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,9 @@ pub mod manajemen_produk;
 pub mod transaksi_penjualan;
 pub mod manajemen_supplier;
 pub mod manajemen_pelanggan;
+
+use rocket_db_pools::Database;
+
+#[derive(Database)]
+#[database("buildingstore")]
+pub struct BuildingStoreDB(sqlx::PgPool);
