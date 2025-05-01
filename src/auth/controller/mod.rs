a89218207c547ab9028ba2ddcf0bf6b110a92471
HamletJr <jogosboy1902@gmail.com>
2025-05-01 17:16:45 +0700

    [REFACTOR] Adjust config in main and controller msg for auth

--- src/auth/controller/mod.rs
+++ src/auth/controller/mod.rs
@@ -3,7 +3,7 @@ use rocket::{fairing::AdHoc, routes};
 pub mod auth;
 
 pub fn route_stage() -> AdHoc {
-    return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
+    return AdHoc::on_ignite("Initializing /api/auth controller routes...", |rocket| async {
         rocket
             .mount("/api/auth", routes![auth::login, auth::register, auth::logout])
     });
