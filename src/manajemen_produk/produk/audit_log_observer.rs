03b44a7166eb8fe8be25ae2413e6343a9d10c997
fachhhh <fachhhhs.01@gmail.com>
2025-05-09 08:08:52 +0700

    [REFACTOR] fix: build audit_log_observer.rs

--- src/manajemen_produk/produk/audit_log_observer.rs
+++ src/manajemen_produk/produk/audit_log_observer.rs
@@ -1,20 +1,22 @@
-use crate::manajemen_produk::produk::events::ProdukObserver;
-use crate::manajemen_produk::produk::model::Produk;
-use rocket_db_pools::sqlx::{self, PgPool};
+use std::fmt::Debug;
+// use rocket_db_pools::sqlx::PgPool;
+use super::model::Produk;
+use super::events::ProdukObserver;
 
-pub struct AuditLogObserver {
-    pub db: PgPool,
+#[derive(Debug)]
+pub struct AuditLogObserver;
+
+impl AuditLogObserver {
+    pub fn new() -> Self {
+        AuditLogObserver
+    }
 }
 
 impl ProdukObserver for AuditLogObserver {
     fn on_stock_changed(&self, produk: &Produk, old_stock: u32) {
-        let _ = sqlx::query!(
-            "INSERT INTO produk_audit_log (produk_id, field, old_value, new_value) VALUES ($1, $2, $3, $4)",
-            produk.id.unwrap(),
-            "stok",
-            old_stock as i32,
-            produk.stok as i32
-        )
-        .execute(&self.db);
+        println!(
+            "AUDIT LOG: Stock changed for product '{}' (ID: {:?}) from {} to {}",
+            produk.nama, produk.id, old_stock, produk.stok
+        );
     }
 }
