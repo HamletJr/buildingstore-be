7740d66a61cf5337df4bb9f1f9994f8fc3e9deb3
fachhhh <fachhhhs.01@gmail.com>
2025-05-09 08:24:32 +0700

    [REFACTOR] chore: remove unused modules from mod.rs

--- src/manajemen_produk/produk/mod.rs
+++ src/manajemen_produk/produk/mod.rs
@@ -1,10 +1,9 @@
 pub mod model;
 pub mod repository;
-pub mod create;
-pub mod read;
-pub mod update;
-pub mod delete;
 pub mod validation;
+pub mod controller;
+pub mod events;
+pub mod audit_log_observer;
 
 #[cfg(test)]
 pub mod tests;
