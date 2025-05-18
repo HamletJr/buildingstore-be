5a50d6078c22c8507a96bf55083d7bb669cf96f6
HamletJr <jogosboy1902@gmail.com>
2025-05-19 00:26:30 +0700

    fix: add fkey to user

--- migrations/2_CreateSessions.sql
+++ migrations/2_CreateSessions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (
     session_key VARCHAR PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id),
     expires_at VARCHAR NOT NULL
 );
--- migrations/test/2_CreateSessions.sql
+++ migrations/test/2_CreateSessions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions (
     session_key TEXT PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id),
     expires_at TEXT NOT NULL
 );
