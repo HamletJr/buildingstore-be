83a8418912ab2150ad91a26223627668d10879ee
bddaax <brenda14705@gmail.com>
2025-05-23 17:40:57 +0700

    [FEAT] Complete CRUD routes for transactions and integrate route_stage in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use dotenvy::dotenv;
 use sqlx::any::install_default_drivers;
 use rocket::State;
 use sqlx::{Any, Pool};
+use rocket_cors::{AllowedOrigins, CorsOptions};
 
 pub mod auth;
 pub mod manajemen_produk;
@@ -35,19 +36,29 @@ async fn test_db(db: &State<Pool<Any>>) -> Option<String> {
 async fn rocket() -> _ {
     dotenv().ok();
 
+    // CORS Configuration
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::some_exact(&[
+            "http://127.0.0.1:3000",
+            "https://your-production-domain.com",
+        ]))
+        .allow_credentials(true)
+        .to_cors()
+        .expect("Failed to create CORS");
+
     install_default_drivers();
     let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db_pool = sqlx::AnyPool::connect(&database_url).await.unwrap();
     sqlx::migrate!()
         .run(&db_pool)
         .await
-        .expect("Failed to run migrations");
-
-    rocket::build()
+        .expect("Failed to run migrations");    rocket::build()
         .manage(reqwest::Client::builder().build().unwrap())
         .manage(db_pool)
+        .attach(cors)
         .attach(BuildingStoreDB::init())
         .attach(auth::controller::route_stage())
         .attach(manajemen_pelanggan::controller::route_stage())
+        .attach(transaksi_penjualan::controller::route_stage())
         .mount("/", routes![index, test_db])
 }
