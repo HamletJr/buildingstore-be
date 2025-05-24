9674cc70d83582b04e63402b80437ab97b04fe79
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-05-25 01:09:07 +0700

    fix: adjusted sql

--- migrations/test/5_CreateSuppliers.sql
+++ migrations/test/5_CreateSuppliers.sql
@@ -1,3 +1,12 @@
+CREATE TABLE suppliers (
+    id VARCHAR(255) PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    jenis_barang VARCHAR(255),
+    jumlah_barang INTEGER NOT NULL DEFAULT 0,
+    resi VARCHAR(255),
+    updated_at DATETIME NOT NULL
+);
+
 CREATE TABLE supplier_transactions (
     id VARCHAR(255) PRIMARY KEY NOT NULL,
     supplier_id VARCHAR(255) NOT NULL,
--- migrations/test/6_CreateSuppliers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE suppliers (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    jenis_barang VARCHAR(255),
-    jumlah_barang INTEGER NOT NULL DEFAULT 0,
-    resi VARCHAR(255),
-    updated_at DATETIME NOT NULL
-);
