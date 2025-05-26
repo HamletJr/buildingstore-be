cf41c1bd0e7452942d453a7fd779afc46659a21c
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2025-05-26 20:21:42 +0700

    [REFACTOR] Fix Sql

--- migrations/test/5_CreateInstallments.sql
+++ migrations/test/5_CreateInstallments.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS installments (
     id TEXT PRIMARY KEY,
     payment_id TEXT NOT NULL,
     amount REAL NOT NULL,
-    payment_date TIMESTAMP NOT NULL,
+    payment_date TEXT NOT NULL,
     FOREIGN KEY (payment_id) REFERENCES payments(id) ON DELETE CASCADE
 );
 
